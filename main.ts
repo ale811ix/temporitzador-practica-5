e0cccee0da1598eee92b662ad6874ee6bf62d5ff
Aleix Altayó <82875781+ale811ix@users.noreply.github.com>
2021-05-14 14:41:12 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 25; index++) {
+        led.unplot(x, y)
+        x += -1
+        if (x == -1) {
+            y += -1
+            x += 5
+        }
+        basic.pause(1000)
+    }
+    basic.showIcon(IconNames.No)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    x += 4
+    y += 4
+})
+let x = 0
+let y = 0
+y = 4
+x = 4
